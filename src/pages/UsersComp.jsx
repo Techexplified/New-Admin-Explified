1a0353491c59ea446e9569f5b08ed78cbd786ecf
Techexplified <tech@explified.com>
2025-11-11 20:41:32 +0530

    resolved the login logic

--- src/pages/UsersComp.jsx
+++ src/pages/UsersComp.jsx
@@ -12,7 +12,7 @@ import { useEffect, useState } from "react";
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await axios.get('https://explified-app.web.app/api/get/users');
+        const res = await axios.get('https://us-central1-explified-app.cloudfunctions.net/api/api/users/users');
         setUsers(res.data.users);
         setFilteredUsers(res.data.users); // Initialize filtered list
       } catch (err) {
